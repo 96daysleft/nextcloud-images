c1409cbdc291637d46102e785a14bb98e10c2a0d
Jerky676 <me@jerky676.com>
2025-02-13 09:30:59 -0600

    Restoring install scripts

--- nextcloud-fpm/configure-scripts/occ-collabora.sh
+++ nextcloud-fpm/configure-scripts/occ-collabora.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 occ_collabora_install() {
   echo '## Configuring Collabora...'
-  # install_app richdocuments
+  install_app richdocuments
 
-  echo "missing rich documents 1"
-
-  # occ config:app:set richdocuments wopi_url --value="${NX_COLLABORA_URL:?"NX_COLLABORA_URL is unset"}"
-  # occ config:app:set richdocuments public_wopi_url --value="${NX_COLLABORA_URL:?"NX_COLLABORA_URL is unset"}"
-  # occ config:app:set richdocuments wopi_allowlist --value="${NX_COLLABORA_ALLOWLIST:?"NX_COLLABORA_ALLOWLIST is unset"}"
+  occ config:app:set richdocuments wopi_url --value="${NX_COLLABORA_URL:?"NX_COLLABORA_URL is unset"}"
+  occ config:app:set richdocuments public_wopi_url --value="${NX_COLLABORA_URL:?"NX_COLLABORA_URL is unset"}"
+  occ config:app:set richdocuments wopi_allowlist --value="${NX_COLLABORA_ALLOWLIST:?"NX_COLLABORA_ALLOWLIST is unset"}"
 }
 
 occ_collabora_remove() {
   echo '## Removing Collabora Configuration...'
-  #remove_app richdocuments
+  remove_app richdocuments
 
-  echo "missing rich documents 1"
-
-  # occ config:app:delete richdocuments wopi_url
-  # occ config:app:delete richdocuments public_wopi_url
-  # occ config:app:delete richdocuments wopi_allowlist
+  occ config:app:delete richdocuments wopi_url
+  occ config:app:delete richdocuments public_wopi_url
+  occ config:app:delete richdocuments wopi_allowlist
 }
